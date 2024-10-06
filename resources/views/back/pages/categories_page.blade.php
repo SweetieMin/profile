3dd3fd93186675891fad5fc7c021ddd3b29e546b
Sweetie Min <thomassmyth.isaac@gmail.com>
2024-10-06 07:16:40 +0700

    txt

--- resources/views/back/pages/categories_page.blade.php
+++ resources/views/back/pages/categories_page.blade.php
@@ -1,113 +1,113 @@
 @extends('back.layout.pages-layout')
 @section('pageTitle', isset($pageTitle) ? $pageTitle : 'Page pageTitle')
 @section('content')
-    
-<div class="page-header">
+
+    <div class="page-header">
+        <div class="row">
+            <div class="col-md-12 col-sm-12">
+                <div class="title">
+                    <h4>Categories</h4>
+                </div>
+                <nav aria-label="breadcrumb" role="navigation">
+                    <ol class="breadcrumb">
+                        <li class="breadcrumb-item">
+                            <a href="{{ route('admin.dashboard') }}">Home</a>
+                        </li>
+                        <li class="breadcrumb-item active" aria-current="page">
+                            Categories
+                        </li>
+                    </ol>
+                </nav>
+            </div>
+        </div>
+    </div>
+
     <div class="row">
-        <div class="col-md-12 col-sm-12">
-            <div class="title">
-                <h4>Categories</h4>
-            </div>
-            <nav aria-label="breadcrumb" role="navigation">
-                <ol class="breadcrumb">
-                    <li class="breadcrumb-item">
-                        <a href="{{ route('admin.dashboard') }}">Home</a>
-                    </li>
-                    <li class="breadcrumb-item active" aria-current="page">
-                        Categories
-                    </li>
-                </ol>
-            </nav>
-        </div>
-    </div>
-</div>
-
-<div class="row">
-    <div class="col-12">
-        <div class="pd-20 card-box mb-30">
-            <div class="clearfix">
-                <div class="pull-left">
-                    <div class="h4 text-blue">
-                        Parent categories
+        <div class="col-12">
+            <div class="pd-20 card-box mb-30">
+                <div class="clearfix">
+                    <div class="pull-left">
+                        <div class="h4 text-blue">
+                            Parent categories
+                        </div>
+                    </div>
+                    <div class="pull-right">
+                        <a href="" class="btn btn-primary btn-sm">Add P. categories</a>
                     </div>
                 </div>
-                <div class="pull-right">
-                    <a href="" class="btn btn-primary btn-sm">Add P. categories</a>
+                <div class="table-responsive mt-4">
+                    <table class="table table-borderless table-striped table-sm">
+                        <thead class="bg-secondary text-white">
+                            <th>#</th>
+                            <th>Name</th>
+                            <th>N. of categories</th>
+                            <th>Actions</th>
+                        </thead>
+                        <tbody>
+                            <tr>
+                                <td>1</td>
+                                <td>Name</td>
+                                <td>4</td>
+                                <td>
+                                    <div class="table-actions">
+                                        <a href="" class="text-primary mx-2">
+                                            <i class="dw dw-edit2"></i>
+                                        </a>
+                                        <a href="" class="text-danger mx-2">
+                                            <i class="dw dw-delete-3"></i>
+                                        </a>
+                                    </div>
+                                </td>
+                            </tr>
+                        </tbody>
+                    </table>
                 </div>
             </div>
-            <div class="table-responsive mt-4">
-                <table class="table table-borderless table-striped table-sm">
-                    <thead class="bg-secondary text-white">
-                        <th>#</th>
-                        <th>Name</th>
-                        <th>N. of categories</th>
-                        <th>Actions</th> 
-                    </thead>
-                    <tbody>
-                        <tr>
-                            <td>1</td>
-                            <td>Name</td>
-                            <td>4</td>
-                            <td>
-                                <div class="table-actions">
-                                    <a href="" class="text-primary mx-2">
-                                        <i class="dw dw-edit2"></i>
-                                    </a>
-                                    <a href="" class="text-danger mx-2">
-                                        <i class="dw dw-delete-3"></i>
-                                    </a>
-                                </div>
-                            </td>
-                        </tr>   
-                    </tbody>
-                </table>
-            </div>
         </div>
-    </div>
 
-    <div class="col-12">
-        <div class="pd-20 card-box mb-30">
-            <div class="clearfix">
-                <div class="pull-left">
-                    <div class="h4 text-blue">
-                        Categories
+        <div class="col-12">
+            <div class="pd-20 card-box mb-30">
+                <div class="clearfix">
+                    <div class="pull-left">
+                        <div class="h4 text-blue">
+                            Categories
+                        </div>
+                    </div>
+                    <div class="pull-right">
+                        <a href="" class="btn btn-primary btn-sm">Add categories</a>
                     </div>
                 </div>
-                <div class="pull-right">
-                    <a href="" class="btn btn-primary btn-sm">Add categories</a>
+                <div class="table-responsive mt-4">
+                    <table class="table table-borderless table-striped table-sm">
+                        <thead class="bg-secondary text-white">
+                            <th>#</th>
+                            <th>Name</th>
+                            <th>Parent categories</th>
+                            <th>Name of posts</th>
+                            <th>Actions</th>
+                        </thead>
+                        <tbody>
+                            <tr>
+                                <td>1</td>
+                                <td>Name</td>
+                                <td>Parent</td>
+                                <td>4</td>
+                                <td>
+                                    <div class="table-actions">
+                                        <a href="" class="text-primary mx-2">
+                                            <i class="dw dw-edit2"></i>
+                                        </a>
+                                        <a href="" class="text-danger mx-2">
+                                            <i class="dw dw-delete-3"></i>
+                                        </a>
+                                    </div>
+                                </td>
+                            </tr>
+                        </tbody>
+                    </table>
                 </div>
-            </div>
-            <div class="table-responsive mt-4">
-                <table class="table table-borderless table-striped table-sm">
-                    <thead class="bg-secondary text-white">
-                        <th>#</th>
-                        <th>Name</th>
-                        <th>Parent categories</th>
-                        <th>Name of posts</th>
-                        <th>Actions</th> 
-                    </thead>
-                    <tbody>
-                        <tr>
-                            <td>1</td>
-                            <td>Name</td>
-                            <td>Parent</td>
-                            <td>4</td>
-                            <td>
-                                <div class="table-actions">
-                                    <a href="" class="text-primary mx-2">
-                                        <i class="dw dw-edit2"></i>
-                                    </a>
-                                    <a href="" class="text-danger mx-2">
-                                        <i class="dw dw-delete-3"></i>
-                                    </a>
-                                </div>
-                            </td>
-                        </tr>   
-                    </tbody>
-                </table>
             </div>
         </div>
     </div>
-</div>
 
 @endsection
